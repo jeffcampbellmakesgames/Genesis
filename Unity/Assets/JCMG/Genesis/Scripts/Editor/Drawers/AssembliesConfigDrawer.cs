bfead126be6f26048b7ad19aa8dd328c94d4eb22
Jeff Campbell <jeff@zapdot.com>
2020-04-25 18:30:49 +0200

    Added new config for whitelisting assemblies
    * Created new config AssembliesConfig to enable users to whitelist which assemblies they want data providers to search through. This can offer both a performance enhancement for reducing the number of places to search via reflection as well as make it possible for multiple GenesisSettings assets to limit the scope of their code generation to specific assemblies. DataProviders that wish to use this feature should implement IConfigurable and use an AssembliesConfig to parse and use the users settings for this run.
    * Created new drawer AssembliesConfigDrawer
    * Updated ScriptableFactoryDataProvider to use this new config and demonstrate how it can optionally limit its reflection searching to specific assemblies.
    * Updated example content and created new WhitelistZeroAssembliesGenesisSettings asset to demonstrate how this feature works. It currently is set to use whiteisting, but specifies zero assemblies, ensuring that it's data providers will not find any types and generate any code.

---
+++ Unity/Assets/JCMG/Genesis/Scripts/Editor/Drawers/AssembliesConfigDrawer.cs
@@ -0,0 +1,83 @@
+﻿using UnityEditor;
+
+namespace JCMG.Genesis.Editor
+{
+	internal sealed class AssembliesConfigDrawer : AbstractSettingsDrawer
+	{
+		/// <summary>
+		/// The display title for this drawer
+		/// </summary>
+		public override string Title => TITLE;
+
+		private readonly AssembliesConfig _config;
+
+		// UI
+		private const string TITLE = "Assemblies";
+		private const string DO_USE_WHITE_LIST_LABEL = "Do Use Whitelist";
+		private const string DO_USE_WHITE_LIST_DESCRIPTION = "If enabled, searching via reflection for Data Providers " +
+		                                                     "will be limited to the array of assemblies below. Otherwise " +
+		                                                     "all loaded assemblies will be searched.";
+
+		private const string ASSEMBLY_WHITE_LIST_LABEL = "Assembly Whitelist";
+		private const string ASSEMBLY_WHITE_LIST_DESCRIPTION = "The comma delimited array of assemblies that searching " +
+		                                                       "via reflection for Data Providers should be limited to.";
+
+		public AssembliesConfigDrawer()
+		{
+			_config = new AssembliesConfig();
+		}
+
+		/// <summary>Initializes any setup for the drawer prior to rendering any GUI.</summary>
+		/// <param name="settings"></param>
+		public override void Initialize(GenesisSettings settings)
+		{
+			base.Initialize(settings);
+
+			_config.Configure(settings);
+		}
+
+		protected override void DrawContentBody(GenesisSettings settings)
+		{
+			// Do Use white-list
+			EditorGUILayout.HelpBox(DO_USE_WHITE_LIST_DESCRIPTION, MessageType.Info);
+			using (new EditorGUILayout.HorizontalScope())
+			{
+				EditorGUILayout.LabelField(DO_USE_WHITE_LIST_LABEL);
+
+				using (var scope = new EditorGUI.ChangeCheckScope())
+				{
+					var newValue = EditorGUILayout.Toggle(_config.DoUseWhitelistOfAssemblies);
+
+					if (scope.changed)
+					{
+						_config.DoUseWhitelistOfAssemblies = newValue;
+
+						EditorUtility.SetDirty(settings);
+					}
+				}
+			}
+
+			// White-Listed Assemblies
+			EditorGUILayout.HelpBox(ASSEMBLY_WHITE_LIST_DESCRIPTION, MessageType.Info);
+			using (new EditorGUI.DisabledScope(!_config.DoUseWhitelistOfAssemblies))
+			{
+				using (new EditorGUILayout.HorizontalScope())
+				{
+					EditorGUILayout.LabelField(ASSEMBLY_WHITE_LIST_LABEL);
+
+					using (var scope = new EditorGUI.ChangeCheckScope())
+					{
+						var newValue = EditorGUILayout.TextField(_config.RawWhiteListedAssemblies);
+
+						if (scope.changed)
+						{
+							_config.RawWhiteListedAssemblies = newValue;
+
+							EditorUtility.SetDirty(settings);
+						}
+					}
+				}
+			}
+		}
+	}
+}
