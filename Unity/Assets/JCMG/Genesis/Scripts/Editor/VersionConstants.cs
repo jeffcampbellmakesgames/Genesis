f647f2f4daff3141e99c276dc09870035ceaf5b5
Jeff Campbell <jeff@zapdot.com>
2020-05-02 13:15:44 +0200

    Bumped package version to v1.2.0

--- Unity/Assets/JCMG/Genesis/Scripts/Editor/VersionConstants.cs
+++ Unity/Assets/JCMG/Genesis/Scripts/Editor/VersionConstants.cs
@@ -1,28 +1,3 @@
-/*
-
-MIT License
-
-Copyright (c) Jeff Campbell
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-*/
-
 namespace JCMG.Genesis.Editor
 {
 	/// <summary>
@@ -33,7 +8,7 @@ namespace JCMG.Genesis.Editor
 		/// <summary>
 		/// The semantic version
 		/// </summary>
-		public const string VERSION = "1.1.1";
+		public const string VERSION = "1.2.0";
 
 		/// <summary>
 		/// The branch of GIT this package was published from.
@@ -43,16 +18,16 @@ namespace JCMG.Genesis.Editor
 		/// <summary>
 		/// The current GIT commit hash this package was published on.
 		/// </summary>
-		public const string GIT_COMMIT = "710c448e10e4fb71a4b459367636a012648387b4";
+		public const string GIT_COMMIT = "1c0c816a3a6eda81d3ef27ad309f59d995fd50b7";
 
 		/// <summary>
 		/// The UTC human-readable date this package was published at.
 		/// </summary>
-		public const string PUBLISH_DATE = "Friday, May 1, 2020";
+		public const string PUBLISH_DATE = "Saturday, May 2, 2020";
 
 		/// <summary>
 		/// The UTC time this package was published at.
 		/// </summary>
-		public const string PUBLISH_TIME = "05/01/2020 20:50:18";
+		public const string PUBLISH_TIME = "05/02/2020 11:13:24";
 	}
 }
