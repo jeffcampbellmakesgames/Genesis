f5902745b46bf68a77815fce8fde383e5bbfa255
Jeff Campbell <jeff@zapdot.com>
2020-08-04 20:39:44 -0400

    v1.2.1
    ## SUMMARY
    This adds additional logging for errors experienced during code generation.
    
    ## ADDED
    * Added logging for exceptions experienced during a code-generation run.

--- Scripts/Editor/VersionConstants.cs
+++ Scripts/Editor/VersionConstants.cs
@@ -8,7 +8,7 @@ namespace JCMG.Genesis.Editor
 		/// <summary>
 		/// The semantic version
 		/// </summary>
-		public const string VERSION = "1.2.0";
+		public const string VERSION = "1.2.1";
 
 		/// <summary>
 		/// The branch of GIT this package was published from.
@@ -18,16 +18,16 @@ namespace JCMG.Genesis.Editor
 		/// <summary>
 		/// The current GIT commit hash this package was published on.
 		/// </summary>
-		public const string GIT_COMMIT = "1c0c816a3a6eda81d3ef27ad309f59d995fd50b7";
+		public const string GIT_COMMIT = "a8d8edcd18f327619e6372ca966ff6e5e9ef4525";
 
 		/// <summary>
 		/// The UTC human-readable date this package was published at.
 		/// </summary>
-		public const string PUBLISH_DATE = "Saturday, May 2, 2020";
+		public const string PUBLISH_DATE = "Wednesday, August 5, 2020";
 
 		/// <summary>
 		/// The UTC time this package was published at.
 		/// </summary>
-		public const string PUBLISH_TIME = "05/02/2020 11:13:24";
+		public const string PUBLISH_TIME = "08/05/2020 00:37:05";
 	}
 }
