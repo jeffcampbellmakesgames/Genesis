007e6ca9128f5a1e939e38e36c17ab77ec6f6843
Jeff Campbell <jeff@zapdot.com>
2020-09-05 07:34:03 -0400

    Updated example content
    * Updated example content to include new scriptable factory examples

---
+++ Unity/Assets/ExampleContent/Scripts/FactoryKeys/ArchetypeID.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using JCMG.Genesis;
+using UnityEngine;
+
+namespace ExampleContent
+{
+	[FactoryKeyFor(typeof(Sprite))]
+	[FactoryKeyFor(typeof(GameObject))]
+	[FactoryKeyFor(typeof(GameObject[]))]
+	[FactoryKeyFor(typeof(List<GameObject>))]
+	[CreateAssetMenu]
+	public sealed class ArchetypeID : ScriptableObject { }
+}
