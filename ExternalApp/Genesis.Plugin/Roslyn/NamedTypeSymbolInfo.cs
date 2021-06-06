59075f42b7b72f0c7af587631fc9e9094ec24807
Jeff Campbell <jeff@zapdot.com>
2021-06-06 12:53:40 -0400

    Added additional caching/helpers to NamedTypeSymbolInfo
    * Added additional overloads and caching to NamedTypeSymbolInfo. Modified existing and new properties to be lazily loaded rather than assigned in constructor.

--- ExternalApp/Genesis.Plugin/Roslyn/NamedTypeSymbolInfo.cs
+++ ExternalApp/Genesis.Plugin/Roslyn/NamedTypeSymbolInfo.cs
@@ -1,7 +1,7 @@
-﻿using System.Collections.Generic;
-using System.Collections.Immutable;
+﻿using Microsoft.CodeAnalysis;
+using System;
+using System.Collections.Generic;
 using System.Linq;
-using Microsoft.CodeAnalysis;
 
 namespace Genesis.Plugin
 {
@@ -16,27 +16,103 @@ namespace Genesis.Plugin
 		public INamedTypeSymbol NamedTypeSymbol { get; }
 
 		/// <summary>
-		/// All <see cref="AttributeData"/> decorated on this <see cref="NamedTypeSymbol"/>
+		/// All <see cref="AttributeData"/> decorated on this <see cref="NamedTypeSymbol"/>.  Lazy-loaded.
 		/// </summary>
-		public IEnumerable<AttributeData> AttributeData { get; }
+		public IEnumerable<AttributeData> AttributeData
+		{
+			get { return _attributeData ??= NamedTypeSymbol.GetAttributes(); }
+		}
 
 		/// <summary>
 		/// All interface <see cref="INamedTypeSymbol"/> instances that this <see cref="NamedTypeSymbol"/> and it's
-		/// base types implement.
+		/// base types implement. Lazy-loaded.
 		/// </summary>
-		public IEnumerable<INamedTypeSymbol> InterfaceTypeSymbols { get; set; }
-		public IEnumerable<ITypeSymbol> BaseTypesAndThis { get; set; }
+		public IEnumerable<INamedTypeSymbol> InterfaceTypeSymbols
+		{
+			get { return _interfaceTypeSymbols ??= NamedTypeSymbol.AllInterfaces; }
+		}
 
 		/// <summary>
-		/// Constructor for <see cref="NamedTypeSymbolInfo"/> that caches all commonly used members on creation.
+		/// All base <see cref="ITypeSymbol"/> instances of <see cref="NamedTypeSymbol"/>, including itself.
+		/// Lazy-loaded.
 		/// </summary>
-		/// <param name="namedTypeSymbol"></param>
+		public IEnumerable<ITypeSymbol> BaseTypesAndThis
+		{
+			get { return _baseTypesAndThis ??= NamedTypeSymbol.GetBaseTypesAndThis(); }
+		}
+
+		/// <summary>
+		/// All public <see cref="ISymbol"/> members of <see cref="NamedTypeSymbol"/>. Lazy-loaded.
+		/// </summary>
+		public IEnumerable<ISymbol> AllPublicMembers
+		{
+			get { return _allPublicMembers ??= NamedTypeSymbol.GetAllMembers(); }
+		}
+
+		/// <summary>
+		/// Full type name for <see cref="NamedTypeSymbol"/>. Lazy-loaded.
+		/// </summary>
+		public string FullTypeName
+		{
+			get
+			{
+				if (string.IsNullOrEmpty(_fullTypeName))
+				{
+					_fullTypeName = NamedTypeSymbol.GetFullTypeName();
+				}
+
+				return _fullTypeName;
+			}
+		}
+
+		/// <summary>
+		/// Full type name for <see cref="NamedTypeSymbol"/>. Lazy-loaded.
+		/// </summary>
+		public string TypeName
+		{
+			get
+			{
+				if (string.IsNullOrEmpty(_typeName))
+				{
+					_typeName = NamedTypeSymbol.Name;
+				}
+
+				return _typeName;
+			}
+		}
+
+		private IEnumerable<AttributeData> _attributeData;
+		private IEnumerable<INamedTypeSymbol> _interfaceTypeSymbols;
+		private IEnumerable<ITypeSymbol> _baseTypesAndThis;
+		private IEnumerable<ISymbol> _allPublicMembers;
+		private string _fullTypeName;
+		private string _typeName;
+
+		/// <summary>
+		/// Constructor for <see cref="NamedTypeSymbolInfo"/>.
+		/// </summary>
 		public NamedTypeSymbolInfo(INamedTypeSymbol namedTypeSymbol)
 		{
 			NamedTypeSymbol = namedTypeSymbol;
-			AttributeData = namedTypeSymbol.GetAttributes();
-			BaseTypesAndThis = namedTypeSymbol.GetBaseTypesAndThis();
-			InterfaceTypeSymbols = BaseTypesAndThis.SelectMany(x => x.AllInterfaces).Distinct();
+		}
+
+		/// <summary>
+		/// Returns true if this <see cref="NamedTypeSymbol"/> is decorated with interface type
+		/// <typeparamref name="T"/>.
+		/// </summary>
+		/// <exception cref="ArgumentException">
+		///     <typeparamref name="T" /> must be an interface, otherwise an exception will be thrown.
+		/// </exception>
+		public bool ImplementsInterface<T>()
+		{
+			if (!typeof(T).IsInterface)
+			{
+				throw new ArgumentException("T must be an Interface.");
+			}
+
+			var interfaceName = typeof(T).Name;
+
+			return ImplementsInterface(interfaceName);
 		}
 
 		/// <summary>
@@ -50,7 +126,7 @@ namespace Genesis.Plugin
 		}
 
 		/// <summary>
-		/// Returns all <see cref="Microsoft.CodeAnalysis.AttributeData"/> for this <see cref="NamedTypeSymbol"/> where
+		/// Returns all <see cref="Microsoft.CodeAnalysis.AttributeData"/> for <see cref="NamedTypeSymbol"/> where
 		/// the attribute class's name matches <paramref name="attributeTypeName"/>.
 		/// </summary>
 		public IEnumerable<AttributeData> GetAttributes(string attributeTypeName)
@@ -59,7 +135,25 @@ namespace Genesis.Plugin
 		}
 
 		/// <summary>
-		///     Returns true if this <see cref="NamedTypeSymbol" /> has an
+		///     Returns true if <see cref="NamedTypeSymbol"/> has <see cref="Attribute" />-derived type
+		///     <typeparamref name="T" />.
+		/// </summary>
+		/// <exception cref="Exception">
+		///     <typeparamref name="T" /> must be an attribute, otherwise an assertion
+		///     will be thrown.
+		/// </exception>
+		public bool HasAttribute<T>()
+		{
+			if (!typeof(Attribute).IsAssignableFrom(typeof(T)))
+			{
+				throw new ArgumentException("T must be assignable to Attribute.");
+			}
+
+			return HasAttribute(typeof(T).Name);
+		}
+
+		/// <summary>
+		///     Returns true if <see cref="NamedTypeSymbol" /> has an
 		/// <see cref="Microsoft.CodeAnalysis.AttributeData" /> with <paramref name="attributeTypeName"/>.
 		/// </summary>
 		public bool HasAttribute(string attributeTypeName)
