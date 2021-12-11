a013eb9b623fc92ad30a437d3ca847423b2a0d0d
Jeff Campbell <jeff@zapdot.com>
2021-12-11 17:09:32 -0500

    Updated ScriptableFactory plugin
    * Modified all existing factories to have optional Odin markup for better usability, readability, and utility
    * Created ISymbolObject as an interface representing a uniquely identified object.
    * Created GenerateSymbolFactoryAttribute as an attribute used to indicate an ISymbolObject  should have a factory generated for it. Modified ScriptableFactoryDataProvider and ScriptableFactoryCodeGenerator to provide this functionality
    * Modified ScriptableFactoryCodeGenerator to have class names generated reflect factory as a part of their name.
    
    ad

---
+++ ExternalApp/Genesis.Unity.Factory.Plugin/DataProviders/SymbolFactoryData.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Genesis.Plugin;
+using Microsoft.CodeAnalysis;
+
+namespace Genesis.Unity.Factory.Plugin
+{
+	/// <summary>
+	/// Code-generation data for a factory for a symbol object.
+	/// </summary>
+	public sealed class SymbolFactoryData : CodeGeneratorData
+	{
+		// Keys
+		private const string VALUE_TYPE = "Factory.ValueType";
+
+		// Format
+		private const string OUTPUT_FILE_PATH_FORMAT = @"Factory\{0}.cs";
+		private const string FINAL_TYPE_NAME_FORMAT = @"{0}SymbolFactory";
+
+		// Template Tokens
+		private const string TYPE_NAME_TOKEN = "${TypeName}";
+		private const string VALUE_FULL_TYPE_TOKEN = "${ValueFullType}";
+
+		// Exceptions
+		private const string EXCEPTION_VALUE = "No type found for Value.";
+
+		public SymbolFactoryData(ITypeSymbol valueSymbol)
+		{
+			this[VALUE_TYPE] = valueSymbol;
+		}
+
+		public string GetValueHumanReadableName()
+		{
+			return GetValueNamedTypeSymbol().GetHumanReadableName();
+		}
+
+		public string GetValueFullTypeName()
+		{
+			return GetValueNamedTypeSymbol().ToString();
+		}
+
+		public ITypeSymbol GetValueNamedTypeSymbol()
+		{
+			if (TryGetValue(VALUE_TYPE, out var result))
+			{
+				return (ITypeSymbol)result;
+			}
+
+			throw new Exception(EXCEPTION_VALUE);
+		}
+
+		public string GetFilename()
+		{
+			return string.Format(OUTPUT_FILE_PATH_FORMAT, GetTypeName());
+		}
+
+		public string ReplaceTemplateTokens(string template)
+		{
+			return template
+				.Replace(TYPE_NAME_TOKEN, GetTypeName())
+				.Replace(VALUE_FULL_TYPE_TOKEN, GetValueFullTypeName());
+		}
+
+		private string GetTypeName()
+		{
+			return string.Format(FINAL_TYPE_NAME_FORMAT, GetValueHumanReadableName());
+		}
+	}
+}
---
+++ ExternalApp/Genesis.Unity.Factory/Contracts/ISymbolObject.cs
@@ -0,0 +1,13 @@
+﻿namespace Genesis.Unity.Factory
+{
+	/// <summary>
+	/// Represents an object that can be uniquely identified by it's <see cref="Symbol"/>.
+	/// </summary>
+	public interface ISymbolObject
+	{
+		/// <summary>
+		/// Represents a unique identifier for this object.
+		/// </summary>
+		string Symbol { get; }
+	}
+}
